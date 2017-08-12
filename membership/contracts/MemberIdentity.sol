89c7d20196a3dbd87262790c23d81d73401ebd5e
jimscarver <jimscarver@gmail.com>
2017-08-12 11:29:53 -0400

    Update MemberIdentity.sol

--- membership/contracts/MemberIdentity.sol
+++ membership/contracts/MemberIdentity.sol
@@ -10,17 +10,17 @@ For rchain membership we will need claimed and verified mappings and may want to
 
 Use case using hashes: 
 1. a membership request is made off chain by supplying rhain with their identity factors kept off chain. 
-   Google form triggers email, sms, postcard with codes of 6 charactes of the hash.
-   The hashed identity (form fields) (with a random string at the end?), contract address and code is sent to the user 
-   for verifying a cliam. 
-2. user activates account with ther hash sent by email associating the email addresses with the member hash and ethereum address. 
+   Google form triggers email for activation, sms, postcard with codes of 6 charactes of the hash.
+   The hashed identity (appended form fields) (with a random string at the end?), contract address is sent to the user by email, 
+   contract address and code for verifying a cliam. 
+2. user activates account with their hash sent by email associating the email addresses with the member hash and ethereum address. 
    A small amount of eth is charged to prevent spam and cover costs.
-3. Then they they invoke a validator method in the contract which adds the code as a claim 
+3. Then they they invoke a claim method in the contract which adds the code as a claim 
    for the member associated with the ethereum address. A small amount of eth is charged to prevent spam and cover costs.
 4. The contract owner (rchain) monitors the log for claims and can add, verify or revoke claims and verifications. 
-   Claim verification may reward the claiment in rhoc.
-5. A member can add or replace an eth address associated with their hash.
-6. Anyone can determin if claims have been berified ny rchain associated with the hash id of an ethereum accopunt.
+   Claim verification may reward the claiment in rhoc. todo
+5. A member can add or replace an eth address associated with their hash. todo
+6. Anyone can determin if claims have been verified by rchain associated with the hash id of an ethereum account.
 */
 // work in progress, simple proof of concept, comment and suggest edits.
 // TODO make Owned? integrate with uPort?
@@ -32,15 +32,16 @@ contract MemberIdentityRegistry {
     //   http://solidity.readthedocs.io/en/develop/common-patterns.html
     //   http://solidity.readthedocs.io/en/develop/security-considerations.html
     // TODO test and debug
-    address public owner = msg.sender;
+    address public owner = msg.sender; //TODO add methods to change owner
     address public wallet; // wallet to hold rhoc rewards and eth commected.
     uint256 public price = .001 ether; // prevent spam, cover costs.
     mapping(address => mapping(bytes32 => uint256)) public verified;
-    mapping(address => mapping(bytes32 => uint256)) claimed;  
+    mapping(address => mapping(bytes32 => bytes32)) public code;
+    mapping(address => mapping(bytes32 => uint256)) claimed;  // TOIDO use data structure for hash and code?
     mapping(address => address) uPortAddress; // future use
     
     event ClaimedEvent(uint256 hash, bytes32 factor, bytes32 code);
-    event Verified(uint256 hash);
+    event VerifiedEvent(uint256 hash);
     
     function setPrice(uint256 value) {
         if(owner != msg.sender) throw;
@@ -53,16 +54,18 @@ contract MemberIdentityRegistry {
     }
     function activate(uint256 hash) payable { // hash sent to email verifies email
         // receive eth fee
-	    // if(msg.value != price) throw;
+	    if(msg.value != price) throw;
 	    if ( ! wallet.send(msg.value) ) throw;
         claimed[msg.sender]["email"] = hash;
         ClaimedEvent(hash, "email", "");
     }
-    function claimIFactor(bytes32 factor, uint256 hash) payable {
+    function claimIFactor(bytes32 factor, bytes32 code) payable {
+         uint256 hash = claimed[msg.sender]["email"]
         // receive eth fee
-	    // if(msg.value != price) throw;
+	    if(msg.value != price) throw;
 	    if ( ! wallet.send(msg.value) ) throw;
         claimed[msg.sender][factor] = hash;
+	code[msg.sender][factor] = code;
         ClaimedEvent(hash, factor, code);
     }
 
