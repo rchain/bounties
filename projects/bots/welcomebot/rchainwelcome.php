883f31edf32cf81867b1657172991b76d37d2466
kitblake <kitblake@infrae.com>
2017-08-27 07:27:33 +0200

    Remove superfluous markup

--- projects/bots/welcomebot/rchainwelcome.php
+++ projects/bots/welcomebot/rchainwelcome.php
@@ -1,6 +1,6 @@
 <?php
 # slack integration to welcome members to channels
-require('rchain/initwelcome.php'); # define apptoken
+require('rchain/initwelcome2.php'); # define apptoken
 #set verbose error reporting
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
@@ -66,7 +66,7 @@ echo "channel=$channel\n\n";
         // start message
         $start="Hi {$name} and welcome. We detected that <https://rchain-ai.slack.com/team/{$inviter}|@{$inviter}> invited you to the <https://rchain-ai.slack.com/archives/{$event->channel}|#{$channel}> channel.";
     } else {
-        $start="Hi {$name} and welcome. We detected that you joined the <https://rchain-ai.slack.com/archives/{$event->channel}|#{$channel}> channel.";
+        $start="Hi {$name} and welcome. We detected that you joined the #{$channel} channel.";
 }
 // set channel specific message
 switch ($channel) {
@@ -76,10 +76,10 @@ switch ($channel) {
         $end="If you're seeing this message then something's funky. Could you please inform admin? Thanks.";
         break;
     case "community":
-        $end="If you'd like to get more involved with RChain there is a group of Activists who are collaborating on epics to shape and organize the Cooperative. Their activities are organized using <https://github.com/rchain/Members|Github tools>. Maybe have a look. If you'd like to participate please sign up by filling out this <https://docs.google.com/forms/d/e/1FAIpQLSecwGUVFNx_Xa_Qsw5bxLnaKstPS8kQnfrUGqpuf22rLDteDg/viewform?fbzx=-4415397049662474000|Activist Registration form>. You can contact <https://rchain-ai.slack.com/team/lapin7|@lapin7> if you have any questions.";
+        $end="If you'd like to get more involved with RChain there is a group of Activists who are collaborating on epics to shape and organize the Cooperative. Their activities are organized using <https://github.com/rchain/Members|Github tools>. Maybe have a look. If you'd like to participate please sign up by filling out this <https://docs.google.com/forms/d/e/1FAIpQLSecwGUVFNx_Xa_Qsw5bxLnaKstPS8kQnfrUGqpuf22rLDteDg/viewform?fbzx=-4415397049662474000|Activist Registration form>. You can contact @lapin7 if you have any questions.";
         break;
     case "identity":
-        $end="Maybe you want to check out the <https://docs.google.com/document/d/1y0uoduAO3qMs9cJ7hmO8jmlvlPDBLm8es85b_wKDB2Q/edit|BYOID (Bring Your Own Identity) Project>. Also there's a weekly meeting, every saturday at 11am New York time, in this <https://zoom.us/j/6853551826|Zoom room>. You can contact <https://rchain-ai.slack.com/team/kitblake|@kitblake> if you have questions.";
+        $end="Maybe you want to check out the <https://docs.google.com/document/d/1y0uoduAO3qMs9cJ7hmO8jmlvlPDBLm8es85b_wKDB2Q/edit|BYOID (Bring Your Own Identity) Project>. Also there's a weekly meeting, every saturday at 11am New York time, in this <https://zoom.us/j/6853551826|Zoom room>. You can contact @kitblake if you have questions.";
         break;
     case "rholang":
         $end="If you're new to Rholang and/or Pi Calculus maybe you want to check out the paper <http://mobile-process-calculi-for-programming-the-new-blockchain.readthedocs.io/en/latest/|Mobile process calculi for programming the blockchain>. In any case you can contact @jimscarver if you have questions.";
@@ -93,7 +93,7 @@ default:
 $text = $start." ".$end;
 
 
-// post the message to user on slack
+// post a direct message to user on slack
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL,"https://slack.com/api/chat.postMessage");
 curl_setopt($ch, CURLOPT_POST, 1);
@@ -102,7 +102,7 @@ curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array(
         'channel' => "@".$name,
         'as_user' => false,
         'username' => "Welcome Bot",
-        'icon_url' => "http://divvydao.org/rchain/welcomemat.png",
+        'icon_url' => "http://divvydao.org/rchain/welcomematicon.png",
         'link_names' => true,
         'text' => $text)));
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
